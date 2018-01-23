78304740577985329a4241ba375c40ea3fbace91
rokibul hasan <rakib063049@gmail.com>
2018-01-23 19:46:02 +0600

    Crawl headline tag from site index page

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -1,2 +1,22 @@
 class Crawler < ApplicationRecord
+  serialize :contents
+
+  validates :site_url, presence: true, uniqueness: true
+
+  before_save :validate_site_url
+  after_create :scrape_site
+
+  private
+
+  def validate_site_url
+    site_uri = URI(self.site_url)
+    if site_uri.scheme.blank?
+      self.site_url = "https://#{self.site_url}"
+    end
+  end
+
+  def scrape_site
+    contents = HtmlTagExtractor.crawl(self.site_url)
+    self.update_column(:contents, contents)
+  end
 end
---
+++ app/services/html_tag_extractor.rb
@@ -0,0 +1,16 @@
+require 'wombat'
+
+class HtmlTagExtractor
+
+  def self.crawl(site_url)
+    Wombat.crawl do
+      base_url site_url
+      path "/"
+
+      h1({xpath: "//h1"}, :list)
+      h2({xpath: "//h2"}, :list)
+      h3({xpath: "//h3"}, :list)
+    end
+  end
+
+end
